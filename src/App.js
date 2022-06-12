73f6903cd62b7830dacba5cc69fad321928ae009
kaeriv93 <vsan93@yahoo.com>
2022-06-12 09:52:41 -0500

    changed up login page css

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import './App.css';
 import Main from './Components/Main';
 import Footer from './Components/Footer';
 import 'react-toastify/dist/ReactToastify.css'
+import Header from './Components/Header';
 
 function App() {
   return (
    <>
-
+    {/* <Header/> */}
     <Main/>
     <Footer/>
    </>
